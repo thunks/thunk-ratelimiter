8b86679f3d65586439daa8acf8ad7f9559fe085f
zensh <admin@zensh.com>
2015-12-02 21:28:38 +0800

    support rate limit policy

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var thunk = require('thunks')()
 var redis = require('thunk-redis')
 var limitScript = fs.readFileSync(__dirname + '/ratelimite.lua', {encoding: 'utf8'})
 
+var slice = Array.prototype.slice
+
 module.exports = Limiter
 
 /**
@@ -56,18 +58,29 @@ Limiter.prototype.connect = function (redisClient) {
 
 Limiter.prototype.get = function (id, max, duration) {
   id = this.prefix + ':' + id
-  max = max > 0 ? max : this.max
-  duration = duration > 0 ? duration : this.duration
+  if (max == null) max = this.max
+  if (duration == null) duration = this.duration
+
+  var rest = slice.call(arguments, 3)
+
   return thunk.call(this, function (done) {
-    this.redis.evalauto(limitScript, 1, id, max, duration, Date.now())(function (err, res) {
+    var args = [limitScript, 1, id, Date.now(), max, duration]
+    // check more pairs of `max, duration`
+    if (rest.length) args.push.apply(args, rest)
+    for (var i = 4, len = args.length; i < len; i += 2) {
+      if (!(args[i] > 0 && args[i + 1] > 0)) throw new Error(args[i] + ' or ' + args[i + 1] + ' invalid')
+    }
+
+    this.redis.evalauto(args)(function (err, res) {
       if (err) throw err
-      return new Limit(res[0], res[1], res[2])
+      return new Limit(res[0], res[1], res[2], res[3])
     })(done)
   })
 }
 
-function Limit (remaining, total, reset) {
+function Limit (remaining, total, duration, reset) {
   this.remaining = remaining
   this.total = total
+  this.duration = duration
   this.reset = reset
 }
--- ratelimite.lua
+++ ratelimite.lua
@@ -1,29 +1,57 @@
 -- KEYS[1] target hash key
--- ARGV[3] max count, expire time, current timestamp
+-- ARGV[n >= 3] current timestamp, max count, duration, max count, duration, ...
 
 -- HASH: KEYS[1]
 --   field:ct(count)
 --   field:lt(limit)
+--   field:dn(duration)
 --   field:rt(reset)
 
-local limit = redis.call('hmget', KEYS[1], 'ct', 'lt', 'rt')
+local policyCount = (#ARGV - 1) / 2
+local ssKey = '{' .. KEYS[1] .. '}:SS'
+local limit = redis.call('hmget', KEYS[1], 'ct', 'lt', 'dn', 'rt')
 local res = {}
 
 if limit[1] then
   res[1] = tonumber(limit[1]) - 1
   res[2] = tonumber(limit[2])
-  res[3] = tonumber(limit[3])
+  res[3] = tonumber(limit[3]) or ARGV[3]
+  res[4] = tonumber(limit[4])
 
-  if res[1] > 0 then
-    redis.call('hincrby', KEYS[1], 'ct', -1)
+  if policyCount > 1 and res[1] == 0 then
+    redis.call('incr', ssKey)
+    redis.call('pexpire', ssKey, res[3] * 2)
   end
-else
-  res[1] = tonumber(ARGV[1])
-  res[2] = res[1]
-  res[3] = tonumber(ARGV[2]) + tonumber(ARGV[3])
 
-  redis.call('hmset', KEYS[1], 'ct', res[1], 'lt', res[1], 'rt', res[3])
-  redis.call('pexpire', KEYS[1], ARGV[2])
+  if res[1] >= 0 then
+    redis.call('hincrby', KEYS[1], 'ct', -1)
+  else
+    res[1] = 0
+  end
+
+else
+
+  local index = 1
+  if policyCount > 1 then
+    index = tonumber(redis.call('get', ssKey)) or 1
+    if index > policyCount then
+      index = policyCount
+    end
+  end
+
+  res[1] = tonumber(ARGV[index * 2])
+  res[2] = res[1]
+  res[3] = tonumber(ARGV[index * 2 + 1])
+  res[4] = tonumber(ARGV[1]) + res[3]
+
+  redis.call('hmset', KEYS[1], 'ct', res[1], 'lt', res[1], 'dn', res[3], 'rt', res[4])
+  redis.call('pexpire', KEYS[1], res[3])
+
+  if policyCount > 1 then
+    redis.call('set', ssKey, index)
+    redis.call('pexpire', ssKey, res[3] * 2)
+  end
+
 end
 
 return res
