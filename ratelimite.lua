7d57da709c0f6e253c141d2e7fad3d8054f7a0e3
Orange Mi <orangemiwj@gmail.com>
2018-09-12 13:24:49 +0800

    remain origin lua keys params

--- ratelimite.lua
+++ ratelimite.lua
@@ -10,9 +10,7 @@
 
 local res = {}
 local policyCount = (#ARGV - 1) / 2
-local statusKey = KEYS[1]
-local countKey = KEYS[2]
-local limit = redis.call('hmget', countKey, 'ct', 'lt', 'dn', 'rt')
+local limit = redis.call('hmget', KEYS[2], 'ct', 'lt', 'dn', 'rt')
 
 if limit[1] then
 
@@ -22,16 +20,16 @@ if limit[1] then
   res[4] = tonumber(limit[4])
 
   if policyCount > 1 and res[1] == -1 then
-    redis.call('incr', statusKey)
-    redis.call('pexpire', statusKey, res[3] * 2)
-    local index = tonumber(redis.call('get', statusKey))
+    redis.call('incr', KEYS[1])
+    redis.call('pexpire', KEYS[1], res[3] * 2)
+    local index = tonumber(redis.call('get', KEYS[1]))
     if index == 1 then
-      redis.call('incr', statusKey)
+      redis.call('incr', KEYS[1])
     end
   end
 
   if res[1] >= -1 then
-    redis.call('hincrby', countKey, 'ct', -1)
+    redis.call('hincrby', KEYS[2], 'ct', -1)
   else
     res[1] = -1
   end
@@ -40,7 +38,7 @@ else
 
   local index = 1
   if policyCount > 1 then
-    index = tonumber(redis.call('get', statusKey)) or 1
+    index = tonumber(redis.call('get', KEYS[1])) or 1
     if index > policyCount then
       index = policyCount
     end
@@ -52,8 +50,8 @@ else
   res[3] = tonumber(ARGV[index * 2 + 1])
   res[4] = tonumber(ARGV[1]) + res[3]
 
-  redis.call('hmset', countKey, 'ct', res[1], 'lt', res[2], 'dn', res[3], 'rt', res[4])
-  redis.call('pexpire', countKey, res[3])
+  redis.call('hmset', KEYS[2], 'ct', res[1], 'lt', res[2], 'dn', res[3], 'rt', res[4])
+  redis.call('pexpire', KEYS[2], res[3])
 
 end
 
